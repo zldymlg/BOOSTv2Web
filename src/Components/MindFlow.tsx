9deb82e6dc5ff2cc45bdb81408bbc6af191a39f2
TroyBignotia <aceboybignotia@gmail.com>
2025-03-26 00:44:57 +0800

    function

--- src/Components/MindFlow.tsx
+++ src/Components/MindFlow.tsx
@@ -1,22 +1,12 @@
 import React from "react";
-import { useState } from "react";
 import "./MindFlow.css";
-import { BsSearch } from "react-icons/bs";
 
-const Mindflow: React.FC = () => {
+const MindFlow: React.FC = () => {
   return (
-    <div className="bg-white-p-8-flex flex-col items-center">
-      <div className="text-2xl font-bold mb-8">MindFlow</div>
-      <div className="flex flex-col items-center space-y-4">
-        <div className="bg-green-500 text-white p-4 rounded-lg w-48 text-center">
-          Idea
-          </div>
-          <div className="flex space-x-8">
-            <div className=      
-    
-
-
+    <div className="mindflow-container">
+      Mind Flow
+    </div>
   );
 };
 
-export default Mindmap;
+export default MindFlow;
